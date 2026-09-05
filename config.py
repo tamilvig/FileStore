da0d697fa58fcc5b0548e4f412be702e0ff9d8da
tamilvig <tamiltunemix@gmail.com>
2026-09-05 08:14:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ DB_NAME = "yatjh"
 
 FSUBS = [[-1004294321324, True, 10]] # Force Subscription Channels [channel_id, request_enabled, timer_in_minutes]
 # Database Channel (Primary)
-DB_CHANNEL =    # just put channel id dont add ""
+DB_CHANNEL = [-1004410404312]  # just put channel id dont add ""
 # Multiple Database Channels (can be set via bot settings)
 # DB_CHANNELS = {
 #     "-1004410404312": {"name": "Primary DB", "is_primary": True, "is_active": True},
